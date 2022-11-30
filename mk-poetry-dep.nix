9d992eaa00e17fca1a15d17f32bdaf63a55fe758
sepiabrown <sepiabrown@naver.com>
2022-11-30 12:05:05 +0900

    mk-poetry-dep wheelUnpack test

--- mk-poetry-dep.nix
+++ mk-poetry-dep.nix
@@ -108,7 +108,7 @@ pythonPackages.callPackage
         hooks.poetry2nixFixupHook
       ]
       ++ lib.optional (!isSource && (getManyLinuxDeps fileInfo.name).str != null) autoPatchelfHook
-      ++ lib.optionals (format == "wheel" && !(lib.hasPrefix "tensorflow-gpu" name) ) [
+      ++ lib.optionals (format == "wheel" && (! (lib.hasPrefix "tensorflow-gpu" name) )) [
         (lib.warnIf (! (lib.hasPrefix "tensorflow-gpu" name)) ("${name} not tensorflow-gpu mkpoetrydep") (hooks.wheelUnpackHook))
         #hooks.wheelUnpackHook
         pythonPackages.pipInstallHook
